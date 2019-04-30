d919f2a88a8540faab133f7d868c6b05d49156ad
qijigui <457237252@qq.com>
2019-04-30 14:52:11 +0800

    first commit

---
+++ microservice/microservice-common/src/main/java/cn/fintecher/pangolin/common/SpringBootCommonApplication.java
@@ -0,0 +1,19 @@
+package cn.fintecher.pangolin.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @Author: qijigui
+ * @CreateDate: 2019/4/30 10:32
+ * @Description:
+ */
+@SpringCloudApplication
+public class SpringBootCommonApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootCommonApplication.class, args);
+
+    }
+}
---
+++ microservice/microservice-provider/src/main/java/cn/fintecher/pangolin/SpringBootProviderApplication.java
@@ -0,0 +1,19 @@
+package cn.fintecher.pangolin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: qijigui
+ * @CreateDate: 2019/4/30 10:53
+ * @Description:
+ */
+@SpringBootApplication
+public class SpringBootProviderApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootProviderApplication.class, args);
+
+    }
+}
